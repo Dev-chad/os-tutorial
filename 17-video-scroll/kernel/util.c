a721b267bcb9d6c4260f054393fa76b6c42a54a3
Yongwoon Lim <dev.chadlim@gmail.com>
2017-10-27 16:01:28 +0900

    modified:   17-video-scroll/kernel/util.c 	added reverse code

--- 17-video-scroll/kernel/util.c
+++ 17-video-scroll/kernel/util.c
@@ -9,15 +9,23 @@ void memory_copy(char *source, char *dest, int nbytes) {
  * K&R implementation
  */
 void int_to_ascii(int n, char str[]) {
-    int i, sign;
+    int i, j, sign;
+    char tmp;
+    
     if ((sign = n) < 0) n = -n;
     i = 0;
     do {
         str[i++] = n % 10 + '0';
     } while ((n /= 10) > 0);
 
-    if (sign < 0) str[i++] = '-';
-    str[i] = '\0';
+    if (sign < 0) str[i] = '-';
 
-    /* TODO: implement "reverse" */
+    /* reverse */
+    for(j=0; j<=i/2; j++){
+	tmp = str[j];
+	str[j] = str[i-j];
+	str[i-j] = tmp;
+    }
+    
+    str[++i] = '\0';
 }
